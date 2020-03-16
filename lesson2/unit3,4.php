76524be7578fb2634fb5f39bee380a095ab0d91f
Vladislav <vl241095@gmail.com>
2020-03-16 21:33:25 +0500

    add new code

--- lesson2/unit3,4.php
+++ lesson2/unit3,4.php
@@ -9,23 +9,30 @@ function multi($arg1, $arg2) {
     return $arg1 * $arg2;
 }
 function div($arg1, $arg2) {
+    if ($arg2 !== 0) {
     return $arg1 / $arg2;
+    } else {
+        echo "На 0 делить нельзя!";
+    }
 }
 function mathOperation($arg1, $arg2, $operation) {
     switch ($operation) {
-    case 'Сумма';
+    case Сумма;
         echo sum($arg1,$arg2);
     break;
-    case 'Разность':
+    case Разность:
         echo diff($arg1,$arg2);
     break;
-    case 'Умножение':
+    case Умножение:
         echo multi($arg1, $arg2);
     break;
-    case 'Деление':
+    case Деление:
         echo div($arg1, $arg2);
     break;
     }
 }
-echo mathOperation(2,5,'Умножение');
+$arg1 = rand(-10,10);
+$arg2 = rand(-10,10);
+echo "Число 1 равно $arg1, число 2 равно $arg2 <hr>";
+echo mathOperation($arg1,$arg2,Деление);
 ?>
--- lesson2/unit6.php
+++ lesson2/unit6.php
@@ -1,3 +1,13 @@
 <?php
-
+function power($val, $pow) {
+    if ($pow == 0) {
+        return 1;
+      }
+      if ($pow < 0) {
+        return power(1/$val, -$pow);
+      }
+      return $val * power($val, $pow-1);
+}
+echo power(3, 3);
 ?>
+
